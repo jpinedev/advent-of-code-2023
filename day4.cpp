548643d4f573403cff5ad4a578973c086d1bc094
Jacob Pine <jpinedev@gmail.com>
2023-12-08 12:03:36 -0500

    completed day4.2

--- day4.cpp
+++ day4.cpp
@@ -18,6 +18,7 @@ typedef array<int, 25> OwnNumbers;
 
 struct Scratchcard
 {
+    size_t id;
     Winners winners;
     OwnNumbers ownNumbers;
 
@@ -33,6 +34,20 @@ struct Scratchcard
         }
 
         return points >> 1; // Compensate for original bit
+    }
+
+    int CountMatches() const
+    {
+        int matches = 0;
+        
+        for (auto number : ownNumbers)
+        {
+            // Linear search should be faster than set operations at this scale
+            if (find(winners.begin(), winners.end(), number) != winners.end())
+                ++matches;
+        }
+
+        return matches;
     }
 };
 
@@ -53,7 +68,8 @@ vector<Scratchcard> ParseInputs()
 
         string _throwAwayInput;
         stringstream stream(line);
-        stream >> _throwAwayInput >> _throwAwayInput; // Card #:
+        stream >> _throwAwayInput >> card.id >> _throwAwayInput; // Card #:
+        --card.id; // # is 1 indexed, we want 0 indexed for easier memoization
 
         for (auto& winner : card.winners)
         {
@@ -81,15 +97,44 @@ void Part1(const vector<Scratchcard> &inputs)
     cout << "Part 1: " << sum << endl;
 }
 
-// void Part2(const vector<string> &inputs)
-// {
+void Part2(const vector<Scratchcard> &inputs)
+{
+    vector<size_t> memo;
+    memo.reserve(inputs.size());
+    vector<int> additionalCardIds;
 
-// }
+    for (auto& card : inputs)
+    {
+        memo.push_back(card.CountMatches());
+
+        size_t numToAppend = memo.back();
+        if (numToAppend == 0)
+            continue;
+        
+        for (size_t ii = card.id + 1; ii < inputs.size() && numToAppend > 0; ++ii, --numToAppend)
+        {
+            additionalCardIds.push_back(ii);
+        }
+    }
+
+    for (size_t ii = 0; ii < additionalCardIds.size(); ++ii)
+    {
+        size_t cardId = additionalCardIds.at(ii);
+        size_t numToAppend = memo.at(cardId);
+        
+        for (size_t ii = 0; ii < numToAppend; ++ii)
+        {
+            additionalCardIds.push_back(cardId + ii + 1);
+        }
+    }
+    
+    cout << "Part 2: " << inputs.size() + additionalCardIds.size() << endl;
+}
 
 int main()
 {
   vector<Scratchcard> inputs = ParseInputs();
   Part1(inputs);
-  // Part2(inputs);
+  Part2(inputs);
   return 0;
 }
